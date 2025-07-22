1a899bda4702b548728da9fc60e2e51f9207a155
Rekiro <dampadofors@gmail.com>
2025-07-22 17:03:53 +0700

    Add support for git tool.uv.sources

--- sdk/python/packages/flet-cli/src/flet_cli/commands/build.py
+++ sdk/python/packages/flet-cli/src/flet_cli/commands/build.py
@@ -1823,6 +1823,16 @@ class Command(BaseCommand):
                 if dev_packages_configured:
                     toml_dependencies.append("--no-cache-dir")
 
+            # replace toml dependencies with git sources from [tool.uv.sources]
+            uv_sources = self.get_pyproject("tool.uv.sources")
+            for i, toml_dep in enumerate(toml_dependencies):
+                if toml_dep in uv_sources:
+                    # now only "git" sources supported
+                    # (but without options such as tag, branch, etc)
+                    if "git" in uv_sources[toml_dep]:
+                        git_source = uv_sources[toml_dep]["git"]
+                        toml_dependencies[i] = f"git+{git_source}"
+
             for toml_dep in toml_dependencies:
                 package_args.extend(["-r", toml_dep])
 
